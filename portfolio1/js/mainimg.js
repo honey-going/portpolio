a0cacd53c51369ff18766b6aa20239a22ccf9818
이헌 <bori5437@naver.com>
2020-09-14 11:47:50 +0900

    add

--- portfolio1/js/mainimg.js
+++ portfolio1/js/mainimg.js
@@ -14,14 +14,14 @@
           //slideheight:9000
        });
        
-         $('.bxslider > div').each(function(){   //1단계           
-             //5단계
+         $('.bxslider > div').each(function(){   //1단계
             if($(window).width() < 740){
                 //alert();
                  var img = $(this).find('img');  //2단계  find ==> 선택자를 찾는 명령
                  var img_pc = img.attr('src');    //3단계  
                  var img_mo = img_pc.replace('imgs','m_img') //4단계   relapce(a,b) ==> a를 b로 대체
                 img.attr('src',img_mo);
+                 
                 
                 
             }else{
@@ -29,10 +29,13 @@
                  var img_mo = img.attr('src');    //3단계  
                  var img_pc = img_mo.replace('m_img','imgs') //4단계   relapce(a,b) ==> a를 b로 대체
                 img.attr('src',img_pc);
-                
+                 ;
+      
+                a = 1;
                 
             }
-    })  
+            
+      })  
        
        /*
        if($(window).width() < 740){
